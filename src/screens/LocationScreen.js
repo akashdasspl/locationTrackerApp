83d91b2eb62d01978263de2f16927440a8dd5c2b
lekhrajsaini0 <lekhrajsaini700@gmail.com>
2023-03-19 22:23:13 +0530

    map added

--- src/screens/LocationScreen.js
+++ src/screens/LocationScreen.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {StatusBar, StyleSheet, Text, View} from 'react-native';
-
+import Geolocation from '@react-native-community/geolocation';
+import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
 function LocationScreen() {
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+  Geolocation.getCurrentPosition(data => setLongitude(data.coords.longitude));
+  Geolocation.getCurrentPosition(data => setLatitude(data.coords.latitude));
+
   return (
     <>
       <StatusBar backgroundColor={'#fff'} barStyle={'dark-content'} />
-
-      <View style={styles.container}>
-        <Text style={{color: '#000'}}>start</Text>
+      <View style={{flex: 1}}>
+        <MapView
+          style={StyleSheet.absoluteFill}
+          provider={PROVIDER_GOOGLE}
+          initialRegion={{
+            latitude: latitude,
+            longitude: longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}></MapView>
       </View>
     </>
   );
