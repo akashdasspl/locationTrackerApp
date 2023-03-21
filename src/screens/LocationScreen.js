8083616b6b4680b1c9113f05c3e3ef12a5391f77
lekhrajsaini0 <lekhrajsaini700@gmail.com>
2023-03-21 23:13:23 +0530

    UiAdded

--- src/screens/LocationScreen.js
+++ src/screens/LocationScreen.js
@@ -1,17 +1,46 @@
 import React, {useEffect, useState} from 'react';
-import {StatusBar, StyleSheet, Text, View} from 'react-native';
+import {
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+  PermissionsAndroid,
+  Alert,
+  Button,
+} from 'react-native';
 import Geolocation from '@react-native-community/geolocation';
 import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
 import {getDatabase, ref, set} from 'firebase/database';
 import {db} from '../../config';
+import DeviceInfo from 'react-native-device-info';
+import {getUniqueId, getManufacturer} from 'react-native-device-info';
 function LocationScreen() {
   const [latitude, setLatitude] = useState('');
   const [longitude, setLongitude] = useState('');
   const [username, setusername] = useState('akash');
-  Geolocation.getCurrentPosition(data => setLongitude(data.coords.longitude));
-  Geolocation.getCurrentPosition(data => setLatitude(data.coords.latitude));
+  const [ip, setIp] = useState('');
+  const [granted, setGranted] = useState(false);
+  const [Dname, setDname] = useState('');
+  const trackLocation = () => {
+    Geolocation.getCurrentPosition(data => setLongitude(data.coords.longitude));
+    Geolocation.getCurrentPosition(data => setLatitude(data.coords.latitude));
+    // Alert.alert(`latitude :${latitude}  \n longitude : ${longitude}`);
+  };
   useEffect(() => {
     sendEvent();
+  });
+  DeviceInfo.getIpAddress().then(ip => {
+    setIp(ip);
+  });
+  const appName = DeviceInfo.getApplicationName();
+  console.log(appName);
+  DeviceInfo.getDeviceName().then(deviceName => {
+    //console.log(deviceName);
+    // setDname(deviceName);
+  });
+  DeviceInfo.getUniqueId().then(uniqueId => {
+    console.log(uniqueId);
+    setDname(uniqueId);
   });
 
   function sendEvent() {
@@ -19,21 +48,50 @@ function LocationScreen() {
       username: username,
       latitude: latitude,
       longitude: longitude,
+      ip: ip,
     });
   }
+  useEffect(() => {
+    requestLocationPermission();
+  }, []);
+  useEffect(() => {
+    if (granted) {
+      trackLocation();
+    }
+  }, [granted]);
+  const requestLocationPermission = async () => {
+    try {
+      if (
+        (await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        )) === PermissionsAndroid.RESULTS.GRANTED
+      ) {
+        console.log('Granted');
+        setGranted(true);
+      } else {
+        Alert.alert('location permission is required.');
+        setGranted(false);
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  };
+
   return (
     <>
       <StatusBar backgroundColor={'#fff'} barStyle={'dark-content'} />
       <View style={{flex: 1}}>
-        <MapView
-          style={StyleSheet.absoluteFill}
-          provider={PROVIDER_GOOGLE}
-          initialRegion={{
-            latitude: latitude,
-            longitude: longitude,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}></MapView>
+        {!granted && (
+          <Button
+            title="Ask permission again"
+            onPress={requestLocationPermission}
+          />
+        )}
+        {granted && (
+          <View style={styles.container}>
+            <Text style={styles.text}>{appName}</Text>
+          </View>
+        )}
       </View>
     </>
   );
@@ -41,4 +99,7 @@ function LocationScreen() {
 export default LocationScreen;
 const styles = StyleSheet.create({
   container: {alignItems: 'center', justifyContent: 'center', flex: 1},
+  text: {
+    alignSelf: 'center',
+  },
 });
